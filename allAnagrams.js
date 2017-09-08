1b8167fec4234ed12b186a90993da5b4353e59a1
Ely Alamillo <el0013@gmail.com>
2017-09-07 22:16:33 -0500

    challenge completed

---
+++ allAnagrams.js
@@ -0,0 +1,27 @@
+/**
+* Given a single input string, write a function that produces all possible anagrams
+* of a string and outputs them as an array. At first, don't worry about
+* repeated strings.  What time complexity is your solution?
+*
+* Extra credit: Deduplicate your return array.
+*/
+/**
+* example usage:
+* const anagrams = allAnagrams('abc');
+* console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+*/
+
+const allAnagrams = (str, start = '') => {
+  if (str.length === 1) return [start + str];
+  const anagrams = [];
+  for (let i = 0; i < str.length; i++) {
+    const result = allAnagrams(str.substr(0, i) + str.substr(i + 1), str[i]);
+    for (let j = 0; j < result.length; j++) {
+      anagrams.push(start + result[j]);
+    }
+  }
+  return anagrams;
+};
+
+const anagrams = allAnagrams('abc');
+console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]  
