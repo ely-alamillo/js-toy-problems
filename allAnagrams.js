ed450c706a33e53e5e01d28076a5e51bfa621b95
Ely Alamillo <ely0013@gmail.com>
2017-10-05 19:34:33 -0500

    completed challenge

--- allAnagrams.js
+++ allAnagrams.js
@@ -1,19 +1,21 @@
 /**
-* Given a single input string, write a function that produces all possible anagrams
-* of a string and outputs them as an array. At first, don't worry about
-* repeated strings.  What time complexity is your solution?
-*
-* Extra credit: Deduplicate your return array.
-*/
-/**
-* example usage:
-* const anagrams = allAnagrams('abc');
-* console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-*/
+ * Given a single input string, write a function that produces all possible anagrams
+ * of a string and outputs them as an array. At first, don't worry about
+ * repeated strings.  What time complexity is your solution?
+ *
+ * Extra credit: Deduplicate your return array.
+ */
 
-const allAnagrams = (str, start = '') => {
+/**
+  * example usage:
+  * const anagrams = allAnagrams('abc');
+  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+  */
+
+  const allAnagrams = (str, start = '') => {
   if (str.length === 1) return [start + str];
   const anagrams = [];
+
   for (let i = 0; i < str.length; i++) {
     const result = allAnagrams(str.substr(0, i) + str.substr(i + 1), str[i]);
     for (let j = 0; j < result.length; j++) {
@@ -22,6 +24,3 @@ const allAnagrams = (str, start = '') => {
   }
   return anagrams;
 };
-
-const anagrams = allAnagrams('abc');
-console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]  
